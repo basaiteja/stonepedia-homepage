b4d07ba38bd571a6a8e034fa8e65ae00704bfd69
basaiteja <138089442+basaiteja@users.noreply.github.com>
2024-12-14 01:36:07 +0530

    s4

--- src/components/MaterialCarousel.jsx
+++ src/components/MaterialCarousel.jsx
@@ -1,11 +1,10 @@
 "use client";
 
 import React from "react";
-import dynamic from "next/dynamic";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-const Slider = dynamic(() => import("react-slick"), { ssr: false });
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules"; // Updated import
+import "swiper/css";
+import "swiper/css/pagination";
 
 const materials = [
   {
@@ -31,77 +30,61 @@ const materials = [
 ];
 
 const MaterialCarousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "0px",
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div
       style={{
-        
         padding: "5vh 30vh 20vh 30vh",
         color: "#fff",
         textAlign: "center",
       }}
     >
-      <Slider {...settings}>
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={2}
+        slidesPerView={5}
+        centeredSlides={true}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1024: { slidesPerView: 5 },
+          768: { slidesPerView: 3 },
+          480: { slidesPerView: 2 },
+        }}
+        style={{ width: "100%" }}
+      >
         {materials.map((material, index) => (
-          <div key={index} style={{ textAlign: "center" }}>
-            <div
-              style={{
-                width: "150px",
-                height: "150px",
-                margin: "0 auto",
-                borderRadius: "50%",
-                background: material.color,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
-              }}
-            ></div>
-            <p
-              style={{
-                marginTop: "10px",
-                fontSize: "16px",
-                fontWeight: "600",
-                color: "#fff",
-              }}
-            >
-              {material.name}
-            </p>
-          </div>
+          <SwiperSlide key={index}>
+            <div style={{ textAlign: "center" }}>
+              <div
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  margin: "0 auto",
+                  borderRadius: "50%",
+                  background: material.color,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
+                }}
+              ></div>
+              <p
+                style={{
+                  marginTop: "20px",
+                  fontSize: "16px",
+                  fontWeight: "600",
+                  color: "#fff",
+                }}
+              >
+                {material.name}
+              </p>
+            </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </div>
   );
 };
